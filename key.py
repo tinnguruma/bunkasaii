94e571c99b217f202b3c2a352917c60437eb8ca3
Tinguruma <90504745+tinnguruma@users.noreply.github.com>
2022-06-17 23:02:52 +0900

    Add files via upload

---
+++ key.py
@@ -0,0 +1,29 @@
+from playsound import playsound
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+END = '\033[0m'
+
+while (True):
+    password = input("password==>")
+    
+    if password == "very sexy":
+        break
+    
+    if password == "4545":
+        print("")
+        print(GREEN + " ___ _   _  ___ ___ ___  ___ ___ " + END)
+        print(GREEN + "/ __| | | |/ __/ __/ _ \/ __/ __|" + END)
+        print(GREEN + "\__ \ |_| | (_| (_|  __/\__ \__ \ " + END)
+        print(GREEN + "|___/\__,_|\___\___\___||___/___/" + END)
+        print("")
+        playsound("key1.mp3")
+    else:
+        print("")
+        print(RED + "  ___ _ __ _ __ ___  _ __ " + END)
+        print(RED + " / _ \ '__| '__/ _ \| '__|" + END)
+        print(RED + "|  __/ |  | | | (_) | |   " + END)
+        print(RED + " \___|_|  |_|  \___/|_|   " + END)
+        print("")
+        playsound("key2.mp3")
+        
